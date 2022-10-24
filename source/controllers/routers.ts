7cd2777cbc59ac942f4277caeb72104daf2f8c85
Sai Kiran Reddy Poreddy <saikiranreddy.saikiran@gmail.com>
2022-10-24 18:16:23 +0530

    Fix steamKey issue

--- source/controllers/routers.ts
+++ source/controllers/routers.ts
@@ -2,15 +2,15 @@
 import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse } from 'axios';
 
-let steamKey:any = process.env.STEAM_KEY;
 
 
 // getting Steam Summary
 const steamSummary = async (req: Request, res: Response, next: NextFunction) => {
 
+    let steamKey:any = process.env.STEAM_KEY;
     let steamId: string = req.params.id;
-    console.log(process.env.STEAM_KEY)
     let result: AxiosResponse = await axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steamId}`);
+    console.log(result)
     let steamSummaryPayload: any = result.data;
     return res.status(200).json({
         data: steamSummaryPayload
@@ -20,6 +20,7 @@ const steamSummary = async (req: Request, res: Response, next: NextFunction) => 
 // get Steam Recently played
 const steamRecentlyPlayed = async (req: Request, res: Response, next: NextFunction) => {
 
+    let steamKey:any = process.env.STEAM_KEY;
     let steamId: string = req.params.id;
     let result: AxiosResponse = await axios.get(`http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${steamKey}&steamid=${steamId}&format=json`);
     let steamRecentlyPlayedPayload: any = result.data;
