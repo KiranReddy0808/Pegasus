2314872e97182d3bbd7b3d043da70cca8068aec2
Sai Kiran Reddy Poreddy <saikiranreddy.saikiran@gmail.com>
2022-10-24 22:56:37 +0530

    SVG Trial

--- source/controllers/routers.ts
+++ source/controllers/routers.ts
@@ -1,6 +1,20 @@
 /** source/controllers/posts.ts */
 import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse } from 'axios';
+import generateSVG from '../svg/generateSVG';
+
+interface SteamData {
+    name : string,
+    picture: any,
+    url: string,
+    recentGames: Array<RecentGame>
+}
+
+interface RecentGame {
+    name: string,
+    picture: string,
+    playTimeTwoWeeks: number
+}
 
 
 
@@ -33,7 +47,7 @@ const dailyCatto = async (req: Request, res: Response, next: NextFunction) => {
 
     let result: AxiosResponse = await axios.get(`https://api.thecatapi.com/v1/images/search`);
     let catto: any = result.data[0].url;
-    let catStream: any = await await axios.get(catto, {responseType: 'arraybuffer'})
+    let catStream: any = await axios.get(catto, {responseType: 'arraybuffer'})
     let catImage: any = Buffer.from(catStream.data)
     res.writeHead(200, {
         'Content-Type': 'image/jpeg',
@@ -44,11 +58,25 @@ const dailyCatto = async (req: Request, res: Response, next: NextFunction) => {
 
 const steamSummarySvg = async (req: Request, res: Response, next: NextFunction) => {
 
-    //let steamId: string = req.params.id;
-    //let result: AxiosResponse = await axios.get(`http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${steamKey}&steamid=${steamId}&format=json`);
-    //let steamRecentlyPlayedPayload: any = result.data;
+    let steamKey:any = process.env.STEAM_KEY;
+    let steamId: string = req.params.id;
+    let summaryResult: AxiosResponse = await axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steamId}`);
+    let recentlyPlayedResult: AxiosResponse = await axios.get(`http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${steamKey}&steamid=${steamId}&format=json`);
+    
+    let steamData: SteamData = {name : '', picture: '', url: '', recentGames: []  };
+    steamData['name'] = summaryResult.data.response.players[0].personaname;
+    steamData['picture'] = Buffer.from(await (await axios.get(summaryResult.data.response.players[0].avatar, {responseType: 'arraybuffer'})).data).toString('base64');
+    steamData['url'] = summaryResult.data.response.players[0].profileurl;
+    
+    for(let recentGame of recentlyPlayedResult.data.response.games) {
+        let gameImage: any =  Buffer.from (await (await axios.get(`http://media.steampowered.com/steamcommunity/public/images/apps/${recentGame.appid}/${recentGame.img_icon_url}.jpg`, {responseType: 'arraybuffer'})).data).toString('base64');
+        steamData['recentGames'].push({name: recentGame.name, picture: gameImage, playTimeTwoWeeks: recentGame.playtime_2weeks })
+    }
+    let steamRecentlyPlayedPayload: any = recentlyPlayedResult.data;
+    let recentlyPlayedPayload: any = summaryResult.data
+    let svg: any = await generateSVG.generatedSVG(recentlyPlayedPayload, steamRecentlyPlayedPayload);
     res.setHeader('content-type', 'image/svg+xml')
-    res.status(200).send('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" height="100" width="100"> <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />Sorry, your browser does not support inline SVG. </svg>')
+    res.status(200).send(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" height="100" width="100"> <image xlink:href ="data:image/png;base64,${steamData['picture']}"/><image xlink:href ="data:image/png;base64,${steamData['recentGames'][0]['picture']}"/><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />Sorry, your browser does not support inline SVG. </svg>`)
 
 }
 
---
+++ source/svg/generateSVG.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+import axios, { AxiosResponse } from 'axios';
+
+
+const generatedSVG = async (recentlyPlayed: any, steamRecentlyPlayed: any) => {
+    let result: any
+
+    return result
+}
+
+export default {generatedSVG}
